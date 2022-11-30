67fe53f325fdc1ded5220fbc7bd35e457e9c2a4d
Antonio Ochavo <Antonio.Ochavo@solera.com>
2022-11-30 11:28:35 +0100

    Forcing Concourse!!

--- src/CleanDotnet7API.Api/Program.cs
+++ src/CleanDotnet7API.Api/Program.cs
@@ -16,7 +16,7 @@ builder.Services.AddHealthChecks();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline. //
 if (app.Environment.IsDevelopment()){}
 
 app.UseSwagger();
